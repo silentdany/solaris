2a91cbecd0021895201ad5e11f3c8e6ace48b9d5
silentdany <dagnicourt.a@gmail.com>
2023-01-22 18:49:59 +0100

    fix: update core "chapter" to "episode"

--- src/utils/data/Core.tsx
+++ src/utils/data/Core.tsx
@@ -7,22 +7,22 @@ export const Core = [
     pdf: 'https://drive.google.com/file/d/1V3xd-TjW6StIfxVxSGW_YLiy_ahNgfBS/view?usp=share_link',
   },
   {
-    title: 'Chapitre 1',
-    anchor: 'core-chapter-1',
+    title: 'Episode 1',
+    anchor: 'core-episode-1',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE1SA-d27ecb39c036b0.jpg',
     pdf: 'https://drive.google.com/file/d/1_PKSetwqUjHzwdvxSVBUOgW2MMRzOq7j/view?usp=share_link',
   },
   {
-    title: 'Chapitre 2',
-    anchor: 'core-chapter-2',
+    title: 'Episode 2',
+    anchor: 'core-episode-2',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE2SA-366aaae62c.jpg',
     pdf: 'https://drive.google.com/file/d/1WQdmGIi8vXUSqzv83Uah_ASNOMERrjvA/view?usp=share_link',
   },
   {
-    title: 'Chapitre 3',
-    anchor: 'core-chapter-3',
+    title: 'Episode 3',
+    anchor: 'core-episode-3',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE3SA-a8468616.jpg',
     pdf: 'https://drive.google.com/file/d/1f7R6-80dJKPR2gVdJEykllZXih3ai62n/view?usp=share_link',
