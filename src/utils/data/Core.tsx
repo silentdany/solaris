137ea90a49b0094d085e6db0ac352cc4285fe825
a.dagnicourt <dagnicourt.a@gmail.com>
2023-01-27 23:16:43 +0100

    data: update core links

--- src/utils/data/Core.tsx
+++ src/utils/data/Core.tsx
@@ -4,21 +4,21 @@ export const Core = [
     anchor: 'core-teaser',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE1ME-695e093e30dc.jpg',
-    pdf: 'https://drive.google.com/file/d/1V3xd-TjW6StIfxVxSGW_YLiy_ahNgfBS/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1mUydNkim7u0go5qk71SBSTN1H3aMr3w6/view?usp=share_link',
   },
   {
     title: 'Episode 1',
     anchor: 'core-episode-1',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE1SA-d27ecb39c036b0.jpg',
-    pdf: 'https://drive.google.com/file/d/1_PKSetwqUjHzwdvxSVBUOgW2MMRzOq7j/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1hWxLteHDJyY8EhPRCiH0lUCPHGxSXtBS/view?usp=share_link',
   },
   {
     title: 'Episode 2',
     anchor: 'core-episode-2',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE2SA-366aaae62c.jpg',
-    pdf: 'https://drive.google.com/file/d/1WQdmGIi8vXUSqzv83Uah_ASNOMERrjvA/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1i0r8FHYvCE788WTrAA74xY2sudFzDfXR/view?usp=share_link',
   },
   {
     title: 'Episode 3',
