8bb28fd6841f13ad9eda7406fe52ad6449fb9002
silentdany <dagnicourt.a@gmail.com>
2023-06-01 09:35:25 +0200

    feat: add CORE6

--- src/utils/data/Core.tsx
+++ src/utils/data/Core.tsx
@@ -24,32 +24,38 @@ export const Core = [
     anchor: 'core-episode-1',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE1SA-d27ecb39c036b0.jpg',
-    pdf: 'https://drive.google.com/file/d/1hWxLteHDJyY8EhPRCiH0lUCPHGxSXtBS/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1hWxLteHDJyY8EhPRCiH0lUCPHGxSXtBS/view?usp=drive_link',
   },
   {
     title: 'Episode 2',
     anchor: 'core-episode-2',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE2SA-366aaae62c.jpg',
-    pdf: 'https://drive.google.com/file/d/1gYtqatgELf0ySayZDAc54ptBSlT94EDo/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1gYtqatgELf0ySayZDAc54ptBSlT94EDo/view?usp=drive_link',
   },
   {
     title: 'Episode 3',
     anchor: 'core-episode-3',
     image:
       'https://storage.googleapis.com/nft-assets/items/CORE3SA-a8468616.jpg',
-    pdf: 'https://drive.google.com/file/d/1zYpUrZKsSrlvbfhB8r-nWagv6w0FYW7V/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1zYpUrZKsSrlvbfhB8r-nWagv6w0FYW7V/view?usp=drive_link',
   },
   {
     title: 'Episode 4',
     anchor: 'core-episode-4',
     image: 'https://storage.googleapis.com/nft-assets/items/CORE4-SA.jpg',
-    pdf: 'https://drive.google.com/file/d/1QsHUoz_IAy6ETbvgfnETd03dzNUVMGU5/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1QsHUoz_IAy6ETbvgfnETd03dzNUVMGU5/view?usp=drive_link',
   },
   {
     title: 'Episode 5',
     anchor: 'core-episode-5',
     image: 'https://storage.googleapis.com/nft-assets/items/CORE5-SA.jpg',
-    pdf: 'https://drive.google.com/file/d/1JeLydV6kWfV3Ds6YpQwcynB8UayxYht5/view?usp=share_link',
+    pdf: 'https://drive.google.com/file/d/1JeLydV6kWfV3Ds6YpQwcynB8UayxYht5/view?usp=drive_link',
+  },
+  {
+    title: 'Episode 6',
+    anchor: 'core-episode-6',
+    image: 'https://storage.googleapis.com/nft-assets/items/CORE6-SA.png',
+    pdf: 'https://drive.google.com/file/d/1xuSwQV8KRD87YJUU6VfdmSj4DwNqFO3m/view?usp=drive_link',
   },
 ];
