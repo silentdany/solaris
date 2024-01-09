78deb482bd1c901de6efa146f66d3c29f668d970
silentdany <dagnicourt.a@gmail.com>
2024-01-09 11:29:46 +0100

    feat: add resources tool iframe page to kiosk

---
+++ src/pages/tools/kiosque/resources/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Index from '../../..';
+import Page from '../../../../layout/Page';
+import { Meta } from '../../../../sections/Meta';
+import { AppConfig } from '../../../../utils/AppConfig';
+
+const Tools = () => {
+  return (
+    <>
+      <Meta
+        title="Ressources"
+        description={'Informez-vous sur les ressources de Star Atlas Labs.'}
+        canonical={`${AppConfig.url}/tools/kiosque/resources`}
+      />
+      <Index>
+        <Page title="Ressources" image="/assets/images/kiosk.webp">
+          <div className="flex w-full flex-col items-center justify-center">
+            <iframe
+              src="https://flipsidecrypto.xyz/Datarunner/ressources-ressources-A-gK6X"
+              title="Ressources"
+              className="h-screen w-full -translate-y-16"
+            />
+          </div>
+        </Page>
+      </Index>
+    </>
+  );
+};
+
+export default Tools;
--- src/utils/BreadcrumbsData.tsx
+++ src/utils/BreadcrumbsData.tsx
@@ -3,6 +3,7 @@ export const bcData = [
   { item: 'tools', title: 'Outils', path: '/tools' },
   { item: 'dock', title: 'Dock', path: '/tools/dock' },
   { item: 'kiosque', title: 'Kiosque', path: '/tools/kiosque' },
+  { item: 'resources', title: 'Ressources', path: '/tools/kiosque/resources' },
   { item: 'armada', title: 'Armada', path: '/tools/armada' },
   { item: 'design', title: 'Design', path: '/design' },
 ];
